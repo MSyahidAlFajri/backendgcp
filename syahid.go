b04042938635a52985f278e992a3f0127307f6d5
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-03 02:25:50 +0700

    Nambah Update dan Delete

--- syahid.go
+++ syahid.go
@@ -41,42 +41,43 @@ func ReturnStringStruct(Data any) string {
 	return string(jsonee)
 }
 
-func GCFUpdateNameGeojson(Mongostring, dbname string, r *http.Request) string {
+func GCFUpdateNameGeojson(Mongostring, dbname, colname string, r *http.Request) string {
 	req := new(Credents)
 	resp := new(LonLatProperties)
+	conn := GetConnectionMongo(Mongostring, dbname)
 	err := json.NewDecoder(r.Body).Decode(&resp)
 	if err != nil {
 		req.Status = strconv.Itoa(http.StatusNotFound)
 		req.Message = "error parsing application/json: " + err.Error()
 	} else {
 		req.Status = strconv.Itoa(http.StatusOK)
-		Ins := UpdateNameGeojson(conn, colname,
-			resp.Coordinates,
+		Ins := UpdateDataGeojson(conn, colname,
 			resp.Name,
 			resp.Volume,
 			resp.Type)
-		req.Message = fmt.Sprintf("%v:%v", "Berhasil Update Data Geo", Ins)
+		req.Message = fmt.Sprintf("%v:%v", "Berhasil Input data", Ins)
 	}
 	return ReturnStringStruct(req)
 }
 
-func GCFDeleteDataGeojson(Mongostring, dbname string, r *http.Request) string {
-	req := new(Credents)
-	resp := new(LonLatProperties)
-	err := json.NewDecoder(r.Body).Decode(&resp)
-	if err != nil {
-				req.Status = strconv.Itoa(http.StatusNotFound)
-				req.Message = "error parsing application/json: " + err.Error()
-			} else {
-				req.Status = strconv.Itoa(http.StatusOK)
-				Ins := DeleteDataGeojson(Mongostring, dbname, context.Background(),
-					LonLatProperties{
-						Type:   resp.Type,
-						Name:   resp.Name,
-						Volume: resp.Volume,
-					})
-				req.Message = fmt.Sprintf("%v:%v", "Berhasil Hapus Data Geo", Ins)
-			}
-
-	return ReturnStringStruct(req)
+func GCFDeleteDataGeojson(Mongostring, dbname, colname string, r *http.Request) string {
+    req := new(Credents)
+    resp := new(LonLatProperties)
+    conn := GetConnectionMongo(Mongostring, dbname)
+    err := json.NewDecoder(r.Body).Decode(&resp)
+    if err != nil {
+        req.Status = strconv.Itoa(http.StatusNotFound)
+        req.Message = "error parsing application/json: " + err.Error()
+    } else {
+        req.Status = strconv.Itoa(http.StatusOK)
+        delResult, delErr := DeleteDataGeojson(conn, colname, resp.Name)
+        if delErr != nil {
+            req.Status = strconv.Itoa(http.StatusInternalServerError)
+            req.Message = "error deleting data: " + delErr.Error()
+        } else {
+            req.Message = fmt.Sprintf("Berhasil menghapus data. Jumlah data terhapus: %v", delResult.DeletedCount)
+        }
+    }
+    return ReturnStringStruct(req)
 }
+
